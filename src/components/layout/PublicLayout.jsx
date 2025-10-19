0ab3e31e6cb0c5ef6efa9baec140fde3cb866985
Sahil7709 <sahiltheurkar3286@gmail.com>
2025-10-19 20:27:31 +0530

    our services

--- src/components/layout/PublicLayout.jsx
+++ src/components/layout/PublicLayout.jsx
@@ -1,22 +1,3 @@
-// import ReactLocationTicker from "react";
-// import LocationTicker from "../layout/LocationTicker";
-// import Navbar from "../layout/Navbar";
-// import Footer from "../layout/Footer";
-// import { Outlet } from "react-router-dom";
-
-// export default function PublicLayout() {
-//   return (
-//     <>
-//       <LocationTicker />
-//       <Navbar />
-//       <main className="min-h-[calc(100vh-150px)]">
-//         <Outlet />
-//       </main>
-//       <Footer />
-//     </>
-//   );
-// }
-
 import React from "react";
 import LocationTicker from "../layout/LocationTicker";
 import Navbar from "../layout/Navbar";
@@ -41,9 +22,9 @@ export default function PublicLayout() {
           )}`}
           target="_blank"
           rel="noopener noreferrer"
-          className="fixed bottom-5 right-5 bg-button text-white p-4 rounded-full shadow-lg hover:bg-green-500 transition-all duration-300 flex items-center justify-center whatsapp-jump"
+          className="fixed bottom-2 right-2 bg-button text-white p-2 rounded-full shadow-lg hover:bg-green-500 transition-all duration-300 flex items-center justify-center whatsapp-jump"
         >
-          <FaWhatsapp size={28} />
+          <FaWhatsapp size={38} />
         </a>
 
       </main>
